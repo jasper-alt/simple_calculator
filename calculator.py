2aa80617bf9cbd38e80a06c4be31557e47a9a8fe
Johnely Sambo <johnely@skillsuplab.nl>
2021-10-14 13:25:00 +0200

    added the john function

--- calculator.py
+++ calculator.py
@@ -4,6 +4,12 @@ def add(x, y):
 ## the sub function here for john
 def sub(x, y):
     return (x - y)
+def div(x, y):
+    return x / y
+def mul(x , y):
+    return x * y
+def john(x, y ):
+    return x * y - x
 ## the mul function here for merhwa
 
 ## the div function here for merhwa
@@ -14,9 +20,11 @@ def main():
         n1 = int(input("select the first number you want to use: "))
         n2 = int(input("select the second number you want to use: "))
         op = input("Select the operator you want: ")
-
         if op == "+":
             result = add(n1, n2)
+            run = False
+        elif op == "f":
+            result = john(n1, n2)
             run = False
         elif op == "-":
             result = sub(n1, n2)
