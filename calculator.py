89e208e57ce12257e748ea55250b753b9ba06116
jasperjumelet <jasperjumelet@hotmail.com>
2021-10-14 12:24:16 +0200

    Added main function

--- calculator.py
+++ calculator.py
@@ -1,1 +1,31 @@
-print("hello world")
+## the add function here for john
+
+## the sub function here for john
+
+## the mul function here for merhwa
+
+## the div function here for merhwa
+
+def main():
+    run = True
+    while run:
+        n1 = int(input("select the first number you want to use: "))
+        n2 = int(input("select the second number you want to use: "))
+        op = input("Select the operator you want")
+
+        if op == "+":
+            result = add(n1, n2)
+            run = False
+        elif op == "-":
+            result = sub(n1, n2)
+            run = False
+        elif op == "*":
+            result = mul(n1, n2)
+            run = False
+        elif op == "/":
+            result = div(n1, n2)
+            run = False
+        else:
+            print("Try again but select a correct operator")
+
+    print(result)
